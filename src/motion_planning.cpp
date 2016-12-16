10d73c94e3df6554cc58882a4b888203398a1643
yu <120156842@qq.com>
2016-12-16 21:51:25 +0800

    readrcdata

--- src/motion_planning.cpp
+++ src/motion_planning.cpp
@@ -135,7 +135,7 @@ pnh_("~/fcu")
 	rcdata_sub_ = n.subscribe<asctec_hl_comm::mav_rcdata>("fcu/rcdata", 1, &TeleopIMU::rcdataCallback, this);
 
 
-    send_acc_ctrl();
+   // send_acc_ctrl();
 }
 
 
@@ -186,6 +186,16 @@ void TeleopIMU::rcdataCallback(const asctec_hl_comm::mav_rcdataConstPtr& rcdata)
 
 
 	}
+
+
+	ROS_INFO("channel 0: %ld", (int)(rcdata->channel[0]));
+	ROS_INFO("channel 1: %ld", (int)(rcdata->channel[1]));
+	ROS_INFO("channel 2: %ld", (int)(rcdata->channel[2]));
+	ROS_INFO("channel 3: %ld", (int)(rcdata->channel[3]));
+	ROS_INFO("channel 4: %ld", (int)(rcdata->channel[4]));
+	ROS_INFO("channel 5: %ld", (int)(rcdata->channel[5]));
+	ROS_INFO("channel 6: %ld", (int)(rcdata->channel[6]));
+	ROS_INFO("channel 7: %ld", (int)(rcdata->channel[7]));
 
 
     llcmd_pub_vel.publish(msg);
