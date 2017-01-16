8d42b67271a11dc1f4f01073ddac368d55d1b3a2
yushu <120156842@qq.com>
2017-01-16 16:21:12 +0800

    im

--- src/motion_planning.cpp
+++ src/motion_planning.cpp
@@ -35,7 +35,7 @@ pnh_("~/fcu")
     //topic
 
 
-	rcdata_sub_ = n.subscribe<asctec_hl_comm::mav_rcdata>("fcu/rcdata", 1, &TeleopIMU::rcdataCallback, this);
+	//rcdata_sub_ = n.subscribe<asctec_hl_comm::mav_rcdata>("fcu/rcdata", 1, &TeleopIMU::rcdataCallback, this);
 
 	gps_custom_sub_ =n.subscribe<asctec_hl_comm::GpsCustom> ("fcu/gps_custom", 1, &TeleopIMU::gpsdataCallback, this);
 
